fd9fae3f647d87b4d5c6c1a3f734dc06cec24e9d
serraguti <pacoserranox@gmail.com>
2023-01-27 18:41:16 +0100

    Add project files.

---
+++ MvcSeriesPersonajesPGS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcSeriesPersonajesPGS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
