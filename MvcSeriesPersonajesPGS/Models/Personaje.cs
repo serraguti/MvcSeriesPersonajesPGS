489e27bc824d894ce491f59d26109b8c887c95c8
serraguti <pacoserranox@gmail.com>
2023-01-27 18:56:31 +0100

    commit

---
+++ MvcSeriesPersonajesPGS/Models/Personaje.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcSeriesPersonajesPGS.Models
+{
+    [Table("PERSONAJES")]
+    public class Personaje
+    {
+        [Key]
+        [Column("IDPERSONAJE")]
+        public int IdPersonaje { get; set; }
+        [Column("NOMBRE")]
+        public string Nombre { get; set; }
+        [Column("IMAGEN")]
+        public string Imagen { get; set; }
+        [Column("IDSERIE")]
+        public int IdSerie { get; set; }
+    }
+}
